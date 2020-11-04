03089eefde692e7e2fdc927556329458a76cfb72
mreetyunjaya <mreetyunjaya@gmail.com>
2020-11-04 23:15:25 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,21 +1,19 @@
-##Docker Installation
 #!/bin/bash
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
-sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main" 
-sudo apt-get update
-sudo apt-get -y install docker-engine 
-# add current user to docker group so there is no need to use sudo when running docker
-sudo usermod -aG docker $(whoami)
+
+apt-get update
+apt install docker.io -y
+apt-get install -y docker-compose
+apt install default-jre -y
+
+#have to relogin as ubuntu user  
+usermod -aG docker ubuntu
+
+# restart new session with docker group
+#newgrp docker
+
 #let docker run when server is restarted
-sudo systemctl enable docker
+systemctl enable docker
+
 #create random password for jenkins user which will be created automatically
 export Jenkins_PW=$(openssl rand -base64 16)
 export JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
